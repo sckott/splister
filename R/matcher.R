65b852e140e88e6b301b12f5fc94694d023e829b
Scott Chamberlain <myrmecocystus@gmail.com>
2015-12-09 18:31:03 +0100

    update matcher, eg to readme

--- R/matcher.R
+++ R/matcher.R
@@ -1,17 +1,63 @@
 #' Match spp list to reference list
 #'
-#' @param spplist Input species list, a vector
-#' @param ref Reference taxon data.frame
+#' @param spplist Input species list, a character vector
+#' @param ref Reference taxon data.frame, or file path
 #' @examples
-#' x <- ""
+#' x <- c('Salmo trutta', 'Oncorhynchus clarkii', 'Salmo Linnaeus',
+#' 'Oncorhynchus clarkii', 'Salvelinus fontinalis', 'Salvelinus confluentus')
 #' y <- system.file("examples", "worms_salmo.csv", package = "splister")
-#' matcher(x, y)
-matcher <- function(spplist, ref) {
+#' dat <- unique(read.csv(y, stringsAsFactors = FALSE))
+#' matcher(spplist = x, ref = dat, against = "scientificName")
+#'
+#' # read from file
+#' x <- system.file("examples", "iucn_dat.csv", package = "splister")
+#' x <- read.csv(x, stringsAsFactors = FALSE)[,-1]
+#' y <- system.file("examples", "worms_sample.csv", package = "splister")
+#' spp <- x$sciname[1:20000L]
+#' res <- matcher(spplist = spp, ref = y, against = "scientificName")
+#' unique(sapply(res, attr, "match"))
+#' df <- data.frame(original = spp, taxon = unlist(res),
+#'    match = sapply(res, attr, "match"))
+#' df[df$match == "replace", ]
+matcher <- function(spplist, ref, against = NULL) {
   UseMethod("matcher")
 }
 
 #' @export
 #' @rdname matcher
-matcher.data.frame <- function(spplist, ref) {
+matcher.character <- function(spplist, ref, against = NULL) {
+  if (!is(ref, "data.frame")) ref <- readr::read_csv(ref)
+  unname(lapply(spplist, function(z) {
+    ex <- ref[ref[[against]] %in% z, against]
+    if (length(ex) == 0) {
+      df <- ref[grep(z, ref[[against]]), ]
+      if (NROW(df) == 0) {
+        structure(NA, match = "not_found")
+      } else if (NROW(df) > 1) {
+        ask(df, z, against)
+      } else {
+        structure(df[, against], match = "replace")
+      }
+    } else {
+      structure(ex, match = "exact")
+    }
+  }))
+}
 
+ask <- function(x, z, against) {
+  message("\n\nMore than match found for '", z, "'!\n
+          Enter rownumber (other inputs will return 'NA'):\n")
+  print(x)
+  take <- scan(n = 1, quiet = TRUE, what = 'raw')
+  if (length(take) == 0) {
+    structure(NA, match = "not_found")
+  } else {
+    if (take %in% seq_len(NROW(x))) {
+      take <- as.numeric(take)
+      message("Input accepted, took row '", take, "'.\n")
+      structure(as.character(x[take, against]), match = "replace")
+    } else {
+      structure(NA, match = "not_found")
+    }
+  }
 }
--- README.md
+++ README.md
@@ -21,11 +21,25 @@ library('splister')
 
 ## Examples
 
-xxx
+
+```r
+x <- read.csv(system.file("examples", "iucn_dat.csv", package = "splister"), stringsAsFactors = FALSE)[,-1]
+y <- system.file("examples", "worms_sample.csv", package = "splister")
+spp <- x$sciname[1:20000L]
+res <- matcher(spplist = spp, ref = y, against = "scientificName")
+df <- data.frame(original = spp, taxon = unlist(res),
+   match = sapply(res, attr, "match"))
+df[df$match == "replace", ]
+
+#>                      original              taxon   match
+#> 2080           Alburnus mento Alburnus mentoides replace
+#> 7080           Bahaba chaptis Alburnus mentoides replace
+#> 12080           Caridina evae Alburnus mentoides replace
+#> 17080 Copadichromis inornatus Alburnus mentoides replace
+```
 
 ## Meta
 
-* Please [report any issues or bugs](https://github.com/ropensci/splister/issues).
+* Please [report any issues or bugs](https://github.com/sckott/splister/issues).
 * License: MIT
-* Get citation information for `splister` in R doing `citation(package = 'splister')`
 * Please note that this project is released with a [Contributor Code of Conduct](CONDUCT.md). By participating in this project you agree to abide by its terms.
